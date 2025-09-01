1aa8586139905cce468589a35f28230066e3a4fb
Isabel Alves <66089395+isalves02@users.noreply.github.com>
2025-09-01 23:29:42 +0000

    Adicionando o padrão SRP no código

---
+++ src/main/java/br/com/mariojp/solid/srp/Tax.java
@@ -0,0 +1,13 @@
+package br.com.mariojp.solid.srp;
+
+public class Tax {
+    private double tax;
+
+    public Tax () {
+        this.tax = 0.10;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+}
